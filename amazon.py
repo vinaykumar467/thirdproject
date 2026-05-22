ef3f5a62a6b1d64c2ae442dbd877f87150caa416
vinaykumar467 <87276033+vinaykumar467@users.noreply.github.com>
2026-05-22 22:14:40 +0530

    python code added

---
+++ amazon.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 5
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print(f"Sum: {sum}, Difference: {difference}, Product: {product}, Quotient: {quotient}")
