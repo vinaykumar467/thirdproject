7763adcb11fc2ab4f17dd3bb0ac722b0455d6921
vinaykumar467 <87276033+vinaykumar467@users.noreply.github.com>
2026-05-23 19:00:44 +0530

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -5,5 +5,6 @@ sum = num1 + num2
 difference = num1 - num2
 product = num1 * num2
 quotient = num1 / num2
+print("result is:")
 
 print(f"Sum: {sum}, Difference: {difference}, Product: {product}, Quotient: {quotient}")
